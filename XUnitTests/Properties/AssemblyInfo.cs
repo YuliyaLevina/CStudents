d3b2d5b5c86cef7e9420275057e91695301c88b2
YuliyaLevina <42233516+YuliyaLevina@users.noreply.github.com>
2018-08-09 11:42:36 +0300

    Add XUnit unit tests

---
+++ XUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("XUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("XUnitTests")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d1d7afe0-8f73-46d4-8c4d-d490938606aa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ XUnitTests/XnitCStudentsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using STUDENTS_MARKS_REPORT;
+using Xunit;
+
+namespace XUnitTests
+{
+    public class XnitCStudentsTests
+    {
+        //[Fact]
+        [Theory]
+        [InlineData(0,20)]
+        [InlineData(100,-10)]
+        public void TestMethod1(int a, int b)
+        {
+            int a1 = a;
+            int b1 = b;
+        }
+    }
+}
