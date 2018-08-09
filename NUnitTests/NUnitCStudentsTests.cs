971fdfcb07ff56ecc53d2e0bea15474136cf43d1
YuliyaLevina <42233516+YuliyaLevina@users.noreply.github.com>
2018-08-09 11:40:04 +0300

    Add NUnit unit tests

---
+++ NUnitTests/NUnitCStudentsTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using NUnit.Framework;
+using STUDENTS_MARKS_REPORT;
+
+namespace NUnitTests
+{
+    [TestFixture]
+    public class NUnitCStudentsTests
+    {
+        #region Tests SetUp & TearDown
+        [SetUp]
+        public void StartTest()
+        {
+            Console.Out.WriteLine("Start test.");
+        }
+
+        [TearDown]
+        public void CompleteTest()
+        {
+            Console.Out.WriteLine("Complete test.");
+        }
+        #endregion
+
+        #region Verify Single Student's Info
+        [TestCase("Kate Ivanova", new int[] { 10, 9, 8, 6, 7 })]
+        [TestCase("Александр Петров", new int[] { 7, 10, 5, 8, 7 })]
+        public void AddRecordTest_VerifySingleStudentName(string name, int[] marks)
+        {
+            // Arrange
+            CStudents students = new CStudents();
+
+            // Act
+            students.AddRecord(name, marks);
+
+            // Assert
+            Assert.AreEqual(name, students.studList[0].studentname, "Wrong student's name is added.");
+        }
+
+        [TestCase("Kate Ivanova", new int[] { 10, 9, 8, 6, 7 })]
+        [TestCase("Александр Петров", new int[] { 7, 10, 5, 8, 7 })]
+        public void AddRecordTest_VerifySingleStudentMarks(string name, int[] marks)
+        {
+            // Arrange
+            CStudents students = new CStudents();
+
+            // Act
+            students.AddRecord(name, marks);
+
+            // Assert
+            Assert.AreEqual(marks, students.studList[0].studentmarks, "Wrong student's marks are added.");
+        }
+
+        [TestCase("Kate Ivanova", new int[] { 10, 9, 8, 6, 7 })]
+        [TestCase("Александр Петров", new int[] { 7, 10, 5, 8, 7 })]
+        public void AddRecordTest_VerifySingleStudentTotalMarks(string name, int[] marks)
+        {
+            // Arrange
+            CStudents students = new CStudents();
+            int totalmarks = 0;
+            for (int i = 0; i < marks.Length; i++)
+            {
+                totalmarks += marks[i];
+            }
+
+            // Act
+            students.AddRecord(name, marks);
+
+            // Assert
+            Assert.AreEqual(totalmarks, students.studList[0].totalmarks, "Wrong student's total marks are added.");
+        }
+        #endregion
+    }
+}
---
+++ NUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("NUnitTests")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2809b02d-dd69-48ca-abda-1c8f873693f7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
